71c898db3488f70e732e4dcaeb882867549d2620
Diego Lopez <diegolopezuncc@gmail.com>
2024-05-01 09:59:19 -0400

    Remove unused images and update image paths added styling accross website.

--- customElements/headerFooterManager.js
+++ customElements/headerFooterManager.js
@@ -4,7 +4,7 @@ class SpecialHeader extends HTMLElement {
         <header>
             <nav>
             <ul>
-                <img src="images/LUL_Logo_Horizontal_Brown.png" alt="logo" id="logo">
+                <img src="images/branding/LUL_Logo_Horizontal_Brown.png" alt="logo" id="logo">
                 <li id="push"><a href="index.html">Home</a></li>
                 <li><a href="about.html">About</a></li>
                 <li><a href="gallery.html">Gallery</a></li>
@@ -16,20 +16,35 @@ class SpecialHeader extends HTMLElement {
     }
 }
 
+class SocialFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="socials">
+            <ul>
+                <img src="images/socialMediaIcons/instagram.svg" alt="instagram" id="instagram">
+                <img src="images/socialMediaIcons/linkedin.svg" alt="linkedin" id="linkedin">
+                <img src="images/socialMediaIcons/email.svg" alt="email" id ="email">
+            </ul>
+        </footer>
+    `;
+    }
+}
+
 class SpecialFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <footer>
-            <ul>
-                <img src="images/instagram.svg" alt="instagram" id="instagram">
-                <img src="images/x-twitter.svg" alt="twitter" id="twitter">
-                <img src="images/email.svg" alt="email" id ="email">
-            </ul>
-            <p>Created by Diego Lopez &copy; 2024</p>
+            <ol>
+            <img src="images/branding/LUL_Logo_Horizontal_Gold.png" alt="goldLogo" id="goldLogo">
+            <p>Site Create and Designed by <a href="https://www.linkedin.com/in/diego-j-lopez/">Diego Lopez</a> &copy; 2024, All Rights Reserved.</p>
+            <p>Site Created for the Beta Phi Chapter of La Unidad Latina, Lambda Upsilon Lambda Fraternity Inc.</p>
+            <p><a href="https://www.launidadlatina.org/">LUL National Site</a></p>
+            </ol>
         </footer>
     `;
     }
 }
 
 customElements.define('special-header', SpecialHeader);
+customElements.define('social-footer', SocialFooter);
 customElements.define('special-footer', SpecialFooter);
--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 // Get the container element (focus on the 'ul' within the 'nav')
 var btnContainer = document.querySelector('nav ul');
 
-// Get all list items (li) within the container
-var btns = btnContainer.querySelectorAll('li');
 
-// Loop through the list items and add click event listener
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    // Remove active class from all list items (prevents duplicates)
-    btns.forEach(function(item) {
-      item.classList.remove("active");
-    });
+if (btnContainer) {
+    // Get all list items (li) within the container
+    var btns = btnContainer.querySelectorAll('li');
+    // Loop through the list items and add click event listener
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            // Remove active class from all list items (prevents duplicates)
+            btns.forEach(function (item) {
+                item.classList.remove("active");
+            });
 
-    // Add the active class to the clicked list item
-    this.classList.add("active");
-  });
+            // Add the active class to the clicked list item
+            this.classList.add("active");
+        });
+    }
+} else {
+    // Handle the case where the nav ul element is not found
+    console.error("nav ul element not found");
 }
